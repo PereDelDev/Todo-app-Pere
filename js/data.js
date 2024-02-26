ba9879d17854e4c0924e9df76406ec42e1fb886c
Pere <pere.pachi9@gmail.com>
2024-02-26 20:10:27 +0100

    Funcion de eliminar una tarea tanto del array como de la seccion terminada

--- js/data.js
+++ js/data.js
@@ -1,5 +1,5 @@
 const tareas = [
-    { idTarea: 1, titulo: 'Estudiar Javascript', prioridad: 'urgente' },
-    { idTarea: 2, titulo: 'Dormir', prioridad: 'diaria' },
-    { idTarea: 3, titulo: 'Salir a comer', prioridad: 'mensual' },
+    { idTarea: 0, titulo: 'Estudiar Javascript', prioridad: 'urgente' },
+    { idTarea: 1, titulo: 'Dormir', prioridad: 'diaria' },
+    { idTarea: 2, titulo: 'Salir a comer', prioridad: 'mensual' },
 ]
--- js/todo.js
+++ js/todo.js
@@ -2,7 +2,7 @@ const formNombrePrioridad = document.querySelector('#formNombrePrioridad')
 const formPrioridad = document.querySelector('#soloPrioridad')
 const formNombre = document.querySelector('#soloNombre')
 const seccion = document.querySelector('.secciontareas')
-let id = 1
+let id = tareas.length
 function pintarTareas(lista, domElement) {
     lista.forEach(objeto => pintarTarea(objeto, domElement))
 }
@@ -14,17 +14,30 @@ function getData(event) {
     let tarea = event.target.nombre.value
     let prioridad = event.target.prioridad.value
     const nuevaTarea = { id: id, titulo: tarea, prioridad: prioridad }
+
+    console.log(nuevaTarea)
     guardarData(tareas, nuevaTarea)
 
 
 }
 
+function borrarTarea(event) {
+    let id = Number(event.target.dataset.id);
+    let posicion = tareas.findIndex(tarea => tarea.id === id)
+    tareas.splice(posicion, 1)
+    padre = event.target.parentNode.parentNode
+    hijo = event.target.parentNode
+    padre.removeChild(hijo)
+    console.log(tareas)
+}
 function pintarTarea(objeto, domElement) {
     const article = document.createElement('article')
     const button = document.createElement('button')
     const h2 = document.createElement('h2')
     h2.textContent = objeto.titulo
+    button.dataset.id = id
     button.textContent = 'Eliminar'
+    button.addEventListener('click', borrarTarea)
     article.classList.add(objeto.prioridad)
     article.append(h2, button)
     domElement.appendChild(article)
@@ -35,10 +48,13 @@ function guardarData(list, objeto) {
     if (resultado === -1) {
         list.push(objeto)
         pintarTarea(objeto, seccion)
+        id++
     } else {
         alert('La Tarea ya existe')
     }
 }
+
+
 
 pintarTareas(tareas, seccion)
 
